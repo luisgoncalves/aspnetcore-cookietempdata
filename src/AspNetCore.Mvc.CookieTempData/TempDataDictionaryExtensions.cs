30f682342b69b6142eda3f62f2323a3a414fdda4
Luís Gonçalves <luis.fgoncalv@gmail.com>
2016-07-29 12:48:55 +0100

    Add documentation to extension method

--- src/AspNetCore.Mvc.CookieTempData/TempDataDictionaryExtensions.cs
+++ src/AspNetCore.Mvc.CookieTempData/TempDataDictionaryExtensions.cs
@@ -2,8 +2,19 @@
 
 namespace AspNetCore.Mvc.CookieTempData
 {
+    /// <summary>
+    /// Utility extensions for <see cref="ITempDataDictionary"/>.
+    /// </summary>
     public static class TempDataDictionaryExtensions
     {
+        /// <summary>
+        /// Gets the value associated with the specified key.
+        /// </summary>
+        /// <typeparam name="T">The expected type for the value</typeparam>
+        /// <param name="tempDataDictionary">The target <see cref="ITempDataDictionary"/>.</param>
+        /// <param name="key">The key whose value to get.</param>
+        /// <param name="value">When this method returns, the value associated with the specified key, if the key is found; otherwise, the default value for the type of the value parameter.</param>
+        /// <returns>true if the dictionary contains an element with the specified key</returns>
         public static bool TryGetValue<T>(this ITempDataDictionary tempDataDictionary, string key, out T value)
         {
             object valueObj;
